b7c60a25232ff4ec4c4041eedad21ceb45e4f382
Abhishek Yonghang <ayongh1@gmail.com>
2020-01-27 18:10:06 -0500

    Access controll patch

--- src/nhamain.js
+++ src/nhamain.js
@@ -19,7 +19,7 @@ dotenv.config();
 
 //Core Config: Make sure To investigate more
 app.use(function(req, res, next) {
-res.header("Access-Control-Allow-Origin"," https://nhaservertest.herokuapp.com");
+res.header("Access-Control-Allow-Origin","https://nhaservertest.herokuapp.com");
 res.header("Access-Control-Allow-Credentials", "true");
 res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
