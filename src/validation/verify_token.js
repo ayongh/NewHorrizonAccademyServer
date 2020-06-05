71838726074a1ea47597fb016c8c48320e78c704
Abhishek Yonghang <ayongh1@gmail.com>
2020-06-05 19:24:25 -0400

    New domain from godaddy added

--- src/validation/verify_token.js
+++ src/validation/verify_token.js
@@ -6,46 +6,55 @@ var loger = require('../logger/logger');
 
 module.exports = async function (req, res, next)
 {
+
+    console.log(req.headers)
+
     var starttime = process.hrtime();
 
-    const token = req.cookies.authToken;
+    var bearerToken;
+    var bearerHeader = req.headers["authorization"];
 
-    if (!token) {
-        var payload = {
-            error:'Access deniened'
-        }
-        loger.log(req,res,401,payload.error,payload,starttime)
-        return res.status(401).send(payload)
+    if (typeof bearerHeader !== 'undefined') {
+        var bearer = bearerHeader.split(" ");
+        bearerToken = bearer[1];
+        req.token = bearerToken;
+        
+    } else {
+        return res.status(403).send({error:"Authentication Token is not defined"});
     }
+
+    console.log(bearerToken)
+    const token = bearerToken;
 
     try
     {
 
-        var decodedToken = jwtDecode(token)
-        var userID = decodedToken._id
-    
-        const user = await User.findOne({_id:userID})
-        
-        if(!user)
-        {
-            var payload = {
-                error:"User cannot found"     
-            }
-            loger.log(req,res,404,payload.error,payload,starttime)
-            return res.status(404).send(payload)
-        }
-    
-        const verified = jwt.verify(token, process.env.TOKEN_SECRET)
-        req.user = verified
-        next()
-
+        var decodedToken = await jwtDecode(token)
+       
     }catch(err)
     {
         var payload = {
             error:'auth invalid Token'       
         }
         loger.log(req,res,500,err,payload,starttime)
-        res.status(500).send()
+        return res.status(500).send({error:"internal critical error"})
     }
+
+    var userID = decodedToken._id
+    
+    const user = await User.findOne({_id:userID})
+    
+    if(!user)
+    {
+        var payload = {
+            error:"User cannot found"     
+        }
+        loger.log(req,res,404,payload.error,payload,starttime)
+        return res.status(404).send(payload)
+    }
+
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET)
+    req.user = verified
+    next()
 
 }
